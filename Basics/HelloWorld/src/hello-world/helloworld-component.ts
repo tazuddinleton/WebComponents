bdbe435e6842756ce2ef4fe9f390b1c180ffab71
Taz Uddin <getleton@gmail.com>
2019-11-23 16:44:35 +0600

    add app component

---
+++ Basics/HelloWorld/src/hello-world/helloworld-component.ts
@@ -0,0 +1,35 @@
+import template from './hello-world/template.html';
+
+
+export class HelloWorld extends HTMLElement {
+  private buttonEl: HTMLElement | null = null;
+  private value = 0;
+  private valueEl: HTMLElement | null = null;
+  constructor() {
+    super();
+    const templateEl = document.createElement('template');
+    templateEl.innerHTML = template;
+
+    const shadow = this.attachShadow({ mode: 'closed' });
+    shadow.appendChild(templateEl.content.cloneNode(true));
+
+    this.valueEl = shadow.getElementById('root_value');
+    this.buttonEl = shadow.getElementById('root_button');
+
+    if (this.valueEl === null || this.buttonEl === null) {
+      throw Error('The template of ' + this.tagName + ' got some missing elements.');
+    }
+
+    this.valueEl.textContent = this.value.toString();
+    this.buttonEl.addEventListener('click', this.handleClick);
+  }
+
+  private handleClick = () => {
+    if (!this.valueEl) {
+      return;
+    }
+    this.value += 1;
+    this.valueEl.textContent = this.value.toString();
+  };
+}
+window.customElements.define('hello-world', HelloWorld);
--- Basics/HelloWorld/src/index.ts
+++ Basics/HelloWorld/src/index.ts
@@ -1,13 +1,30 @@
+import {HelloWorld} from './hello-world/helloworld-component';
 
-class HelloWorld extends HTMLElement{
+
+const tmpl = `
+    <style>
+        h1{
+            color: red;
+            font-size: 2em;
+        }
+        .wall{
+            background: lightgray;
+            padding: .2em;
+            text-align: center;
+        }
+    </style>
+    <div class="wall">
+        <h1>Web Components App</h1>            
+    </div>
+`
+
+class AppComponent extends HTMLElement{
     constructor(){
         super();
-
-        const shadow = this.attachShadow({mode: 'closed'});
-        const rootEl = document.createElement('div');
-        rootEl.innerHTML = 'Hello World!';
-        shadow.appendChild(rootEl);
-        
+        const shadow = this.attachShadow({mode: 'open'});
+        const el = document.createElement('div');
+        el.innerHTML = tmpl;
+        shadow.appendChild(el);        
     }
 }
-window.customElements.define('hello-world', HelloWorld);
+window.customElements.define('app-component', AppComponent);
